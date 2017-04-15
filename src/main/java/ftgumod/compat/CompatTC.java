01a9bca2d262646441d696a92d630aa9881d9223
TheRActivator <arievast@hotmail.nl>
2017-04-15 13:13:15 +0200

    Fix for #13

--- src/main/java/ftgumod/compat/CompatTC.java
+++ src/main/java/ftgumod/compat/CompatTC.java
@@ -1,25 +1,29 @@
 package ftgumod.compat;
 
-import ftgumod.workbench.FTGUCraftResult;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Slot;
-import net.minecraft.inventory.SlotCrafting;
-import slimeknights.tconstruct.tools.common.inventory.ContainerCraftingStation;
+/*
+	import ftgumod.workbench.FTGUCraftResult;
+	import net.minecraft.entity.player.EntityPlayer;
+	import net.minecraft.inventory.Slot;
+	import net.minecraft.inventory.SlotCrafting;
+	import slimeknights.tconstruct.tools.common.inventory.ContainerCraftingStation;
+*/
 
 public class CompatTC implements ICompat {
 
 	@Override
 	public boolean run(Object... arg) {
-		if (arg[0] instanceof ContainerCraftingStation && arg[1] instanceof EntityPlayer) {
-			ContainerCraftingStation inv = (ContainerCraftingStation) arg[0];
-			EntityPlayer player = (EntityPlayer) arg[1];
-
-			Slot slot = inv.inventorySlots.get(0);
-			inv.craftResult = new FTGUCraftResult(player);
-			inv.inventorySlots.set(0, new SlotCrafting(player, inv.craftMatrix, inv.craftResult, 0, slot.xPos, slot.yPos));
-			
-			return true;
-		}
+		/*
+			if (arg[0] instanceof ContainerCraftingStation && arg[1] instanceof EntityPlayer) {
+				ContainerCraftingStation inv = (ContainerCraftingStation) arg[0];
+				EntityPlayer player = (EntityPlayer) arg[1];
+	
+				Slot slot = inv.inventorySlots.get(0);
+				inv.craftResult = new FTGUCraftResult(player);
+				inv.inventorySlots.set(0, new SlotCrafting(player, inv.craftMatrix, inv.craftResult, 0, slot.xPos, slot.yPos));
+				
+				return true;
+			}
+		*/
 		return false;
 	}
 
