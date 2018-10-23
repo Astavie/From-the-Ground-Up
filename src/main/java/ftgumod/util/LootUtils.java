79d80ac874a0cdee1ae8cbd7fb950e51eff7d078
Astavie <theractivator@gmail.com>
2018-10-23 16:38:37 +0200

    Fix loot field

--- src/main/java/ftgumod/util/LootUtils.java
+++ src/main/java/ftgumod/util/LootUtils.java
@@ -4,24 +4,14 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootPool;
 import net.minecraft.world.storage.loot.LootTable;
 import net.minecraft.world.storage.loot.LootTableManager;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 import java.lang.reflect.Field;
 import java.util.List;
 
 public class LootUtils {
 
-	private static final Field field;
-
-	static {
-		Field f = null;
-		try {
-			f = LootTable.class.getDeclaredField("pools");
-			f.setAccessible(true);
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
-		}
-		field = f;
-	}
+	private static final Field field = ReflectionHelper.findField(LootTable.class, "pools", "field_186466_c");
 
 	public static void addLootPools(LootTableManager manager, LootTable table, ResourceLocation pools) {
 		LootTable extra = manager.getLootTableFromLocation(pools);
